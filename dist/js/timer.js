67ea158039449ebf8079a328f1005d743e403c25
Olexiy <olexiy@Olexiys-MacBook-Pro.local>
2018-10-31 18:02:52 +0200

    add changes to time.js

--- dist/js/timer.js
+++ dist/js/timer.js
@@ -23,5 +23,20 @@ window.onload = function() {
     mainClass: "mfp-fade",
     removalDelay: 160,
     preloader: !1,
-    fixedContentPos: !1
+    fixedContentPos: !1,
+
+    iframe: {
+      markup: '<div class="mfp-iframe-scaler">' +
+        '<div class="mfp-close"></div>' +
+        '<iframe id="player" class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+        '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+// Converts Youtube links to embeded videos in Magnific popup.
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0&showinfo=0&enablejsapi=1'
+        }
+      }
+    }
 });
